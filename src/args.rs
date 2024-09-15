e1e3fd83bad69ad662637dc8721fdadfc0990574
kaoru <k@warpnine.io>
2024-09-15 13:53:10 +0900

    fix: remove default target

--- src/args.rs
+++ src/args.rs
@@ -7,7 +7,7 @@ use clap::Parser;
 #[clap(author, version, about)]
 pub struct Args {
     /// Array of IP address and port number (IP:port) to ping, delimited by a space
-    #[arg(default_value = "192.168.205.10:2152", value_delimiter = ' ')]
+    #[arg(value_delimiter = ' ')]
     pub target_ips: Vec<SocketAddr>,
     /// Number of pings to send
     #[arg(short, long, default_value = "5")]
